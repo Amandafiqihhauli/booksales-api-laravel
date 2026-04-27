4b4d0b194b34ab2c9d3201694ea57b6be80d023d
Amandafiqihhauli <piki.cou@gmail.com>
2026-04-27 22:40:19 +0700

    Add admin middleware and protect API routes

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,27 @@ use App\Http\Controllers\BookController;
 use App\Http\Controllers\AuthorController;
 use App\Http\Controllers\GenreController;
 
-Route::get('/books', [BookController::class, 'index']);
+// User authentication routes
+Route::get('/authors', [AuthorController::class, 'index']);
+Route::get('/authors/{id}', [AuthorController::class, 'show']);
 
-Route::apiResource('authors', AuthorController::class);
-Route::apiResource('genres', GenreController::class);
+Route::get('/genres', [GenreController::class, 'index']);
+Route::get('/genres/{id}', [GenreController::class, 'show']);
+
+Route::get('/books', [BookController::class, 'index']);
+Route::get('/books/{id}', [BookController::class, 'show']);
+
+// Admin routes for managing authors, genres, and books
+Route::middleware(['auth', 'admin'])->group(function () {
+    Route::post('/authors', [AuthorController::class, 'store']);
+    Route::put('/authors/{id}', [AuthorController::class, 'update']);
+    Route::delete('/authors/{id}', [AuthorController::class, 'destroy']);
+
+    Route::post('/genres', [GenreController::class, 'store']);
+    Route::put('/genres/{id}', [GenreController::class, 'update']);
+    Route::delete('/genres/{id}', [GenreController::class, 'destroy']);
+
+    Route::post('/books', [BookController::class, 'store']);
+    Route::put('/books/{id}', [BookController::class, 'update']);
+    Route::delete('/books/{id}', [BookController::class, 'destroy']);
+});
